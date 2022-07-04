385633e14e2d9c3f9aae4439170ef2ec00d05031
JaimeBucarey19 <bucarey19jaime@hotmail.com>
2022-07-04 01:12:45 -0400

    Primer commit en rama desarrollo

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'url' => $faker->url,
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::name('posts_path')->get('/posts','PostsController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hola/{nombre}','HolaController@hola');
+Route::name('post_path')->get('/posts/{post}', 'PostsController@show');
